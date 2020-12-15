7c920b7984df20d7781cfc4b6403b8fa671e9110
Samuel Figueira Aguiar <RFLMNaguiar@gmail.com>
2020-12-15 16:47:13 -0300

    1.2.0.1
    - Adicionado Estatística.SQL

---
+++ SQL/estatisticas.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 15-Dez-2020 às 20:45
+-- Versão do servidor: 10.4.13-MariaDB
+-- versão do PHP: 7.4.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `st_botanik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estatisticas`
+--
+
+CREATE TABLE `estatisticas` (
+  `id` int(11) DEFAULT NULL,
+  `escolha_a` int(11) DEFAULT NULL,
+  `escolha_b` int(11) DEFAULT NULL,
+  `escolha_c` int(11) DEFAULT NULL,
+  `escolha_d` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
